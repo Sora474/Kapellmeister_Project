4650876feecd6aedf353ffe38d67ebe27bb0debb
Sora <Brazhnikov010@gmail.com>
2023-01-10 16:58:45 +0500

    Add icon prokect

--- Kapellmeister/app/src/main/java/com/example/kapellmeister/MainActivity.kt
+++ Kapellmeister/app/src/main/java/com/example/kapellmeister/MainActivity.kt
@@ -2,10 +2,14 @@ package com.example.kapellmeister
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.kapellmeister.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    lateinit var BindingClass : ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        BindingClass = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(BindingClass.root)
     }
 }
