94f7b12b1641539ee3db1250a05a12f68d3ec3ce
Sora 0 <Brazhnikov010@gmail.com>
2023-01-11 20:46:47 +0500

    Add generic menu

--- Kapellmeister/app/src/main/java/com/example/kapellmeister/MainActivity.kt
+++ Kapellmeister/app/src/main/java/com/example/kapellmeister/MainActivity.kt
@@ -2,6 +2,8 @@ package com.example.kapellmeister
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
 import com.example.kapellmeister.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -11,5 +13,20 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         BindingClass = ActivityMainBinding.inflate(layoutInflater)
         setContentView(BindingClass.root)
+
+        BindingClass.bnvGeneric.selectedItemId = R.id.generic_menu_main_page // Флаг меню по дефолту
+
+
+    }
+
+    fun bnv_generic_Click (view: View){ // Подгрузга страницы в
+        BindingClass.bnvGeneric.setOnItemSelectedListener {
+            when(it.itemId){
+                R.id.generic_menu_main_page          -> { Toast.makeText(this,"mmp",Toast.LENGTH_SHORT).show() }
+                R.id.generic_menu_settings_page      -> { Toast.makeText(this,"msp",Toast.LENGTH_SHORT).show() }
+                R.id.generic_menu_library_music_page -> { Toast.makeText(this,"mlm",Toast.LENGTH_SHORT).show() }
+            }
+            true
+        }
     }
 }
