8c2860b882bd8763398406fd594423e4717a5b02
Sora 0 <Brazhnikov010@gmail.com>
2023-01-14 01:36:46 +0500

    Add Fragment

--- Kapellmeister/app/src/main/java/com/example/kapellmeister/MainActivity.kt
+++ Kapellmeister/app/src/main/java/com/example/kapellmeister/MainActivity.kt
@@ -2,8 +2,11 @@ package com.example.kapellmeister
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
+import android.view.Menu
 import android.widget.Toast
+import com.example.kapellmeister.Pages.AuthorPage
+import com.example.kapellmeister.Pages.LibraryMusicPage
+import com.example.kapellmeister.Pages.ListPage
 import com.example.kapellmeister.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -12,19 +15,49 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         BindingClass = ActivityMainBinding.inflate(layoutInflater)
+        setTheme(R.style.Theme_Kapellmeister)
         setContentView(BindingClass.root)
 
-        BindingClass.bnvGeneric.selectedItemId = R.id.generic_menu_main_page // Флаг меню по дефолту
+        BindingClass.bnvGeneric.selectedItemId = R.id.generic_menu_list_page // Флаг меню по дефолту
 
-
-    }
-
-    fun bnv_generic_Click (view: View){ // Подгрузга страницы в
         BindingClass.bnvGeneric.setOnItemSelectedListener {
             when(it.itemId){
-                R.id.generic_menu_main_page          -> { Toast.makeText(this,"mmp",Toast.LENGTH_SHORT).show() }
-                R.id.generic_menu_settings_page      -> { Toast.makeText(this,"msp",Toast.LENGTH_SHORT).show() }
-                R.id.generic_menu_library_music_page -> { Toast.makeText(this,"mlm",Toast.LENGTH_SHORT).show() }
+                R.id.generic_menu_list_page          -> {
+                    supportFragmentManager
+                        .beginTransaction().replace(R.id.fl_generic, ListPage())
+                        .commit()
+                }
+                R.id.generic_menu_author_page        -> {
+                    supportFragmentManager
+                        .beginTransaction().replace(R.id.fl_generic, AuthorPage())
+                        .commit()
+                }
+                R.id.generic_menu_library_music_page -> {
+                    supportFragmentManager
+                        .beginTransaction().replace(R.id.fl_generic, LibraryMusicPage())
+                        .commit()
+                }
+            }
+            true
+        }
+    }
+
+    ///////////////// Не Menu
+
+    fun bnv_generic_Click (menu: Menu){ // Подгрузга страниц в Frame
+        BindingClass.bnvGeneric.setOnItemSelectedListener {
+            when(it.itemId){
+                R.id.generic_menu_list_page          -> {
+                    supportFragmentManager
+                        .beginTransaction().replace(R.id.fl_generic, ListPage())
+                        .commit()
+                }
+                R.id.generic_menu_author_page        -> {
+                    Toast.makeText(this,"2",Toast.LENGTH_SHORT).show()
+                }
+                R.id.generic_menu_library_music_page -> {
+                    Toast.makeText(this,"3",Toast.LENGTH_SHORT).show()
+                }
             }
             true
         }
